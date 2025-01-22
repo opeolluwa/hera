d7ffa890a0ea125a37b567cc27b52ba3c5a9933d
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-01-22 20:46:49 +0100

    set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'drizzles',
+    //   password: 'thunderstorm',
+    //   database: 'rungo_dev',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
